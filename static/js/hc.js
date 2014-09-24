8d109b30f39c764cd898c48162f56613a62dc0d8
spf13 <steve.francia@gmail.com>
2014-09-24 15:48:47 -0400

    merging in the new design

---
+++ static/js/hc.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $(".container").hide();
+    $(".container").fadeIn('5000');
+    $(".showcase-wrapper").hide();
+    $(".showcase-wrapper").fadeIn("slow");
+});
+
+
+/*
+var toggle = false;
+$('.nav-toggle').on('click', function () {
+    if (toggle == false) {
+        $('#sidebar-wrapper').stop().animate({
+            'left': '4px'
+        });
+        toggle = true;
+    } else {
+        $('#sidebar-wrapper').stop().animate({
+            'left': '250px'
+        });
+        toggle = false;
+    }
+});
+*/
+
+$(function() {
+    $('.project-box').click(function() {
+        $(this).find('.post').slideToggle();
+    });
+});
+
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
